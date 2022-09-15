635951b80a6ba71675cacbfafeed5f9d73576aaa
Ibarsuk <64261965+Ibarsuk@users.noreply.github.com>
2022-09-15 21:06:47 +0300

    jwt env config

---
+++ config/jwt.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'keys' => [
+        'access' => env('JWT_ACCESS_KEY', '9g#fa94hg'),
+        'refresh' => env('JWT_REFRESH_KEY', 'nh295vkwh'),
+    ],
+
+    'algorithm' => 'HS256',
+];
